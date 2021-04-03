4b93698b950285d1d97146efaef8126c1f7779d7
kupeck <70723820+kupeck@users.noreply.github.com>
2021-04-03 12:43:34 +0200

    Interface update

--- en-kupecks_start.bat
+++ en-kupecks_start.bat
@@ -1,25 +1,23 @@
 @echo off
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Copyright start.bat                     I
-echo I   kupeck#9974 2020                        I
-echo I   Some rights reserved                    I
-echo I                                           I
+echo +-----------------Copyright-----------------+
 echo +-------------------------------------------+
+echo     Copyright start.bat                      
+echo     kupeck#9974 2020                         
+echo     Some rights reserved                     
 timeout /T 1
 goto :firsttime
 
 :firsttime
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   First time?                             I
-echo I   1 = New setup                           I
-echo I   2 = Load from previous                  I
-echo I                                           I
+echo +------------------Startup------------------+
 echo +-------------------------------------------+
-set /P _firsttime= Please enter an input: 
+echo     First time?                              
+echo     1 = New setup                            
+echo     2 = Load from previous                   
+set /P _firsttime= Choose an option: 
 if "%_firsttime%"=="1" goto :setup
 if "%_firsttime%"=="2" goto :load
 goto :firsttime
@@ -33,30 +31,27 @@ goto :start
 :setup
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Here write name of your .jar file       I
-echo I   (without .jar)                          I
-echo I   and than press return/enter             I
-echo I                                           I
+echo +-------------------Setup-------------------+
 echo +-------------------------------------------+
+echo     Here write name of your .jar file        
+echo     (without .jar)                           
+echo     and than press return/enter              
 set /P _jarname= Please enter an input: 
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   How much max ram MB allowed?            I
-echo I   (without M)                             I
-echo I   and than press return/enter             I
-echo I                                           I
+echo +-------------------Setup-------------------+
 echo +-------------------------------------------+
+echo     How much max ram MB allowed?             
+echo     (without M)                              
+echo     and than press return/enter              
 set /P _Xmx= Please enter an input: 
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   How much min ram MB allocated?          I
-echo I   (without M)                             I
-echo I   and than press return/enter             I
-echo I                                           I
+echo +-------------------Setup-------------------+
 echo +-------------------------------------------+
+echo     How much min ram MB allocated?           
+echo     (without M)                              
+echo     and than press return/enter              
 set /P _Xms= Please enter an input: 
 if exist "startbat" goto :save
 mkdir "startbat"
@@ -69,70 +64,63 @@ goto :start
 :2ndstart
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Jar name:      %_jarname%                I
-echo I   Allowed RAM:   %_Xmx%                     I
-echo I   Allocated RAM: %_Xms%                     I
-echo I                                           I
+echo +-----------------Your setup----------------+
 echo +-------------------------------------------+
+echo     Jar name:      %_jarname%                 
+echo     Allowed RAM:   %_Xmx%                      
+echo     Allocated RAM: %_Xms%                      
 java -Xmx"%_Xmx%"M -Xms"%_Xms%"M -jar "%_jarname%".jar -nogui
 echo +-------------------------------------------+
 timeout /T 120
 :vyber
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   1 = End program                         I
-echo I   2 = Start the server again              I
-echo I   3 = New setup                           I
-echo I                                           I
+echo +----------------Server ended---------------+
 echo +-------------------------------------------+
-set /P _vybernumber= Please enter an input: 
-if "%_vybernumber%"=="1" goto :konec
+echo     1 = End program (4 = Shutdown)           
+echo     2 = Start the server again               
+echo     3 = New setup                            
+set /P _vybernumber= Choose an option: 
+if "%_vybernumber%"=="1" goto :end
 if "%_vybernumber%"=="2" goto :start
-if "%_vybernumber%"=="3" goto :setup
-if "%_vybernumber%"=="4" start microsoft-edge:"https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?modestbranding=1&rel=0&controls=0&disablekb=1"
+if "%_vybernumber%"=="3" goto :setup 
+if "%_vybernumber%"=="4" goto :end
+if "%_vybernumber%"=="7" start "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?modestbranding=1&rel=0&controls=0&disablekb=1"
 goto :vyber
 
-:konec
+:end
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Safely ended!                           I
-echo I   You can now safely turn off             I
-echo I   your computer                           I
-echo I                                           I
+echo +---------------Program ended---------------+
 echo +-------------------------------------------+
-timeout /T 10
+echo     Safely ended!                            
+echo     You can now safely turn off              
+echo     your computer                            
+timeout /T 10 
+if "%_vybernumber%"=="1" shutdown /f /t 2
 goto :exit
 
 :start
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Server will start soon!                 I
-echo I   In 3 seconds                            I
-echo I                                           I
-echo I                                           I
+echo +------------------Starting-----------------+
 echo +-------------------------------------------+
+echo     Server will start soon!                  
+echo     In 3 seconds                             
 timeout /T 1
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Server will start soon!                 I
-echo I   In 2 seconds                            I
-echo I                                           I
-echo I                                           I
+echo +------------------Starting-----------------+
 echo +-------------------------------------------+
+echo     Server will start soon!                  
+echo     In 2 seconds                             
 timeout /T 1
 cls
 echo +-------------------------------------------+
-echo I                                           I
-echo I   Server will start soon!                 I
-echo I   In 1 second                             I
-echo I                                           I
-echo I                                           I
+echo +------------------Starting-----------------+
 echo +-------------------------------------------+
+echo     Server will start soon!                  
+echo     In 1 second                             
 timeout /T 1
 goto :2ndstart
 
